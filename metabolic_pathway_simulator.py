d174b556fb328099efbb9be71f1c80f08d16a544
Tsimech2000 <juan.ortega5237@gmail.com>
2025-03-16 00:58:32 -0400

    Update metabolic_pathway_simulator.py

--- metabolic_pathway_simulator.py
+++ metabolic_pathway_simulator.py
@@ -41,8 +41,10 @@ def ai_optimization():
     # Set up Genetic Algorithm with DEAP
     if not hasattr(creator, "FitnessMax"):
         creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+creator.create("FitnessMax", base.Fitness, weights=(1.0,))
 
 if not hasattr(creator, "Individual"):
+    creator.create("Individual", list, fitness=creator.FitnessMax)
     creator.create("Individual", list, fitness=creator.FitnessMax)
     toolbox = base.Toolbox()
     toolbox.register("attr_float", random.uniform, 0.1, 5.0)
